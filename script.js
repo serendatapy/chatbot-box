bb7c4ac772ce81fb96648d885a9c1eb14a51de4a
serendatapy <arvaldivia@gmail.com>
2020-05-20 22:53:23 +0200

    added gitignore file, and updated all code to latest changes

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
 $(document).ready(function(){
   
- var messageId = 0;
+ let messageId = 0; //to allow messages to have unique styling
+ const humanColor = "#ffa992"; //speech bubble colors
+ const botColor = "#e8f5f8";
+ const listOfBotAnswers = ["No", "Yes", "I don't think so…", "Of course!", "Indubitably", "In your dreams", "As I see it, yes", "Better not tell you now"];
 
+  const sendMessage = () => { //When message is sent
+            
+      if($("textarea").val().trim().length > 0){  
+        let newMessage = $("textarea").val();
+        let chatHistory = $("#chat-space").html();
+        let bubbleColor = humanColor; //human color
+    
+        createMessage(newMessage,chatHistory,bubbleColor);
+    
+        setTimeout(function(){ //bot response after a delay
+                let botAnswer = generateBotAnswer();
+                bubbleColor = botColor; //bot colour
+                chatHistory = $("#chat-space").html();
+                createMessage(botAnswer,chatHistory,bubbleColor);
+        },1000);
+      }
+   }//end send message
 
-  var sendMessage = function(){
+  const getTime = () => {
+        
+    let currentTime = new Date();
+    let hours= currentTime.getHours();
+    let minutes= currentTime.getMinutes();
+
+    if (hours < 10) {hours = "0" + hours;}
+    if (minutes < 10) {minutes = "0" + minutes;}
     
-    var newMessage = $("textarea").val();
-    var chatHistory = $("#content").html();
-    var backcolor = "#ffa992";
-    createMessage(newMessage,chatHistory,backcolor);
-    
-    setTimeout(function(){
-                var newBotResponse = generateAnswer();
-                backcolor = "#e8f5f8";
-                chatHistory = $("#content").html();
-                createMessage(newBotResponse,chatHistory,backcolor);
-    },1000);
+    return hours + ":" + minutes;
   }
 
-
-  var createMessage = function(newMessage,chatHistory,backgroundColor){
+  const createMessage = (newMessage,chatHistory,backgroundColor) =>{
     
-    //get current time
-    var currentTime = new Date();
-    var hours= currentTime.getHours();
-    var minutes= currentTime.getMinutes();
+    let timeStamp = getTime();   
+    let chatSpace = $("#chat-space");
+    let speechSide = backgroundColor == botColor ? "left" : "right";
+    let drawBubble = speechSide == "left" ? "5px 30px 30px" : "30px 5px 30px 30px";
 
-    if (hours < 10){
-          hours = "0" + hours;
-     }
-    if (minutes < 10){
-          minutes = "0" + minutes;
-     }
-     
-
-    var textarea = $("#content");
-    
-    var side = backgroundColor == "#e8f5f8" ? "left" : "right";
-    var borderSide = side == "left" ? "5px 30px 30px" : "30px 5px 30px 30px"
-
-    //alert(side);
-    textarea.html(chatHistory+ `<p id = ${messageId} >` + newMessage + '<br>' +'<span>'+ " " + hours + ":"+ minutes +'</span>' +'</p>');
-    textarea.scrollTop(textarea[0].scrollHeight);
-    $("#"+messageId).css({"background-color" : backgroundColor, "justify-self" : side, "border-radius": borderSide});
-    $("textarea").val(''); //reset input box
-    messageId++;
-  }
   
+    chatSpace.html(chatHistory + `<p id = ${messageId} >` + newMessage + '<br>' +'<span>'+ timeStamp +'</span>' +'</p>'); //add new message
+
+    $("#"+messageId).css({"background-color" : backgroundColor, "justify-self" : speechSide, "border-radius": drawBubble}); //style the new message
+
+    chatSpace.scrollTop(chatSpace[0].scrollHeight); //scroll to bottom
+
+    $("textarea").val(''); //clear input box
+    messageId++; //generate new id for future messages
+  }
+
+  const generateBotAnswer = () => {    
+      let randomAnswerIndex = Math.floor(Math.random() * listOfBotAnswers.length);
+      return listOfBotAnswers[randomAnswerIndex];
+  };
+  
+//EVENT LISTENERS Click on Button or Enter keypress
+
+  $('button').hover(function(){$(this).toggleClass('buttonHover');});
+
   $("button").click(sendMessage);
 
   $(document).on('keypress',function(e) {
     if(e.which == 13) {
         sendMessage();
     }
-  }); //end enter key listener
-
-  var listOfAnswers = ["No", "Yes", "I don't think so…", "Of course!", "Indubitably", "In your dreams."];
-
-  var generateAnswer = function(){
-            
-            var randomNumber = Math.random();
-            var randomNumberForListOfAnswers = randomNumber * listOfAnswers.length;
-            var randomIndex = Math.floor(randomNumberForListOfAnswers);
-            var answer = listOfAnswers[randomIndex];
-            /*answer = listOfAnswers[0];     */
-            return answer;
-        };
-  
-  $('button').hover(function(){$(this).toggleClass('buttonHover');});
-
+  });
 
 }); //end document.ready
